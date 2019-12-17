cdf846ac3cb0a7fd674af7882d70460ebb04337f
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2019-12-17 19:01:23 -0300

    Salvando novo cliente

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -3,6 +3,7 @@ import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS
 import { Observable } from 'rxjs/Rx';
 import { StorageService } from '../services/storage.service';
 import { AlertController } from 'ionic-angular';
+import { FieldMessage } from '../models/fieldmessage';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
@@ -34,7 +35,11 @@ export class ErrorInterceptor implements HttpInterceptor {
                 case 403:
                     this.handle403();
                     break;
-                
+
+                case 422:
+                    this.handle422(errorObj);
+                    break;
+
                 default:
                     this.handleDefaultError(errorObj);
 
@@ -64,6 +69,21 @@ export class ErrorInterceptor implements HttpInterceptor {
         alert.present();
     }
 
+    handle422(errorObj){
+        let alert = this.alertController.create({
+            title: 'Erro 422: Validação',
+            message: this.listErrors(errorObj.errors),
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+            ]
+        });
+
+        alert.present();
+    }
+
     handleDefaultError(errorObj){
         let alert = this.alertController.create({
             title: "Erro " + errorObj.status + ": " + errorObj.error,
@@ -77,6 +97,16 @@ export class ErrorInterceptor implements HttpInterceptor {
         });
 
         alert.present();
+    }
+
+    private listErrors(messages: FieldMessage[]): string{
+        let s : string = '';
+        
+        for(var i=0; i<messages.length; i++){
+            s = s + '<p><strong>' + messages[i].fieldName + "</strong>: " + messages[i].message + '</p>';
+        }
+
+        return s;
     }
 
 }
