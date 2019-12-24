3fefc2309e91ccbe0768eeee7275d44f7d8ce191
Lucas Bortolatto <lucas-bortolatto@outlook.com>
2019-12-24 14:36:48 -0300

    Componente de Loading

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ProdutoDTO } from '../../models/produto.dto';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { API_CONFIG } from '../../config/api.config';
@@ -21,19 +21,28 @@ export class ProdutosPage {
  
   items : ProdutoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    public produtoService: ProdutoService,
+    public loadingCtrl: LoadingController) {
   }
 
   ionViewDidLoad() {
 
     let categoria_id = this.navParams.get('categoria_id');
 
+    let loader = this.presentLoading(); //apresenta o loading e guarda a referencia dele
+
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response =>{
         this.items = response['content'];
+        loader.dismiss(); //dispensa o loading através da referência
         this.loadImageUrls();
       },
-      error => {});
+      error => {
+        loader.dismiss();
+      });
 
   }
 
@@ -52,6 +61,19 @@ export class ProdutosPage {
 
   showDetail(produto_id: string){
     this.navCtrl.push('ProdutoDetailPage', {produto_id: produto_id});
+  }
+
+  presentLoading(){
+
+    let loader = this.loadingCtrl.create({
+      content: "Aguarde..."/*,
+      duration: 3000*/
+    });
+
+    loader.present();
+
+    //retornando o loader para manipular dinamicamente a sua duração
+    return loader;
   }
 
 }
